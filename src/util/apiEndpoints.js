619edcb81169ba63cc63767cd13642544dd4e5e2
subarnashekhar <subarnashekhar@gmail.com>
2025-12-03 15:50:09 -0600

    changed base url

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
-const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://expense-tracker-subarna.onrender.com/api/v1.0"
+const CLOUDINARY_CLOUD_NAME = "123456";
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
